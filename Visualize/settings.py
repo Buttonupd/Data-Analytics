595525dc4d1a20cf0bd462f195733032c3378a56
Buttonupd <Buttonupdan1199@gmail.com>
2021-08-02 12:19:34 +0300

    Initial config files_15

--- Visualize/settings.py
+++ Visualize/settings.py
@@ -123,7 +123,7 @@ if config('MODE')=="dev":
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
            'HOST': config('DB_HOST'),
-           'PORT': '',
+           'PORT': config('PORT'),
        }
 
    }
